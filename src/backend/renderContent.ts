6d5ec677aceac4d574a7cbcd507bfffeafbf3b0c
CavieJohnsonHere <caviejohnsonhere@gmail.com>
2025-07-30 15:25:54 +0330

    added: an api call to render content (WIP)

---
+++ src/backend/renderContent.ts
@@ -0,0 +1,7 @@
+const renderContent = (page: string) => {
+  console.log(page);
+
+  return new Response("WIP")
+}
+
+export default renderContent;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { serve } from "bun";
 import index from "./index.html";
+import renderContent from "./backend/renderContent";
 
 const server = serve({
   routes: {
     // Serve index.html for all routes.
-    "/*": index
+    "/*": index,
+
+    // API to recive the content of different
+    "/content/:page": (req) => renderContent(req.params.page)
   },
 
   development: process.env.NODE_ENV !== "production",
