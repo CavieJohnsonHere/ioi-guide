90935a6a6891cafcc9ceabc15fd87e321df67ade
CavieJohnsonHere <caviejohnsonhere@gmail.com>
2025-08-01 16:16:22 +0330

    added: secondary mode for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,25 @@
-export default function Button({ children }: { children: React.ReactNode }) {
+export default function Button({
+  children,
+  className,
+  secondary,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  secondary?: boolean;
+}) {
+  const primaryClasses = [
+    `bg-pink-900 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 ${className}`,
+    `bg-pink-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 ${className}`,
+  ];
+
+  const secondaryClasses = [
+    `bg-gray-900 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 ${className}`,
+    `bg-gray-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 ${className}`,
+  ];
+
   return (
-    <button className="bg-pink-900 hover:[&>div]:-translate-y-1 rounded-2xl text-white outline-offset-4">
-      <div className="bg-pink-700 transition-all p-2 px-8 rounded-2xl -translate-y-0.5 active:!translate-0">
-        {children}
-      </div>
+    <button className={secondary ? secondaryClasses[0] : primaryClasses[0]}>
+      <div className={secondary ? secondaryClasses[1] : primaryClasses[1]}>{children}</div>
     </button>
   );
 }
