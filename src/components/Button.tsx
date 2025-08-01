af38c3e11c9ae569f95f20643df7e5c559261bd3
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-08-01 18:10:25 +0330

    changed: the button to have a cursor-pointer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,18 +8,20 @@ export default function Button({
   secondary?: boolean;
 }) {
   const primaryClasses = [
-    `bg-pink-900 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 ${className}`,
-    `bg-pink-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 ${className}`,
+    `bg-pink-900 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 cursor-pointer ${className}`,
+    `bg-pink-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 cursor-pointer ${className}`,
   ];
 
   const secondaryClasses = [
-    `bg-gray-900 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 ${className}`,
-    `bg-gray-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 ${className}`,
+    `bg-gray-800 hover:[&>div]:-translate-y-1.5 rounded-2xl text-white outline-offset-4 cursor-pointer ${className}`,
+    `bg-gray-700 transition-all p-2 px-8 rounded-2xl -translate-y-1 active:!translate-0 cursor-pointer ${className}`,
   ];
 
   return (
     <button className={secondary ? secondaryClasses[0] : primaryClasses[0]}>
-      <div className={secondary ? secondaryClasses[1] : primaryClasses[1]}>{children}</div>
+      <div className={secondary ? secondaryClasses[1] : primaryClasses[1]}>
+        {children}
+      </div>
     </button>
   );
 }
