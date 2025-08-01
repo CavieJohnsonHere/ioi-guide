8257e61363de38c05ed20b9d2151090001c6d0a2
CavieJohnsonHere <caviejohnsonhere@gmail.com>
2025-08-01 16:17:22 +0330

    added: a sample home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,21 @@
+import Button from "./Button";
+
 export default function HomePage() {
   return (
-    <span className="text-red-950"> HomePage works! </span>
-  )
+    <div className="py-10">
+      <h1 className="text-3xl font-bold flex gap-3 justify-center dir-ltr">
+        <img src="/assets/logo.svg" alt="" className="size-16" />
+        <div className="leading-16 bg-clip-text text-white">.guide</div>
+      </h1>
+
+      <div className="text-gray-300 w-128 text-center mx-auto mt-5">
+        The easiest way to learn Combinatorics, made by passionate teachers, for passionate learners.
+      </div>
+
+      <div className="mt-4 flex justify-center gap-5">
+        <Button>Start Learning</Button>
+        <Button secondary>See what's up</Button>
+      </div>
+    </div>
+  );
 }
